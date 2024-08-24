99e3b71fa1eb1a3c736c55e33d61fdac9d6a4a5b
f0590307 <f0590307@gmail.com>
2024-08-24 18:15:28 +0500

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = [4, 'столбец', True]
+values_dict = {'a': 18, 'b': 'белка', 'c': False}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
